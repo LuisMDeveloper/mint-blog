b44fbcf68b1aeb1f73ff7ccd034e3da6269a2fdc
Ramirez Vargas, Luis Manuel <LRamirezVargas@luxoft.com>
2020-05-18 23:03:12 -0500

    basic bootstrap home page

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,8 +67,9 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription
         }
-      ].concat(meta)}
-    />
+      ].concat(meta)}>
+      <script src="https://kit.fontawesome.com/71255fa6bc.js" crossOrigin="anonymous"/>
+    </Helmet>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,53 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
+var faker = require("faker")
+const elements = ["1", "2", "3", "4", "5", "6"]
 const IndexPage = () => (
   <Layout>
     <SEO title="Home"/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image/>
+    <div className="profile-jumbotron">
+      <div className="container">
+        <div className="profile-image">
+          <img src="https://placekitten.com/300/300" alt="placeholder" className="img-thumbnail rounded-circle"/>
+        </div>
+        <div className="profile-overview">
+          <h1 className="display-4">Luis Manuel Ramirez Vargas</h1>
+          <p>iOS software developer with seven years experience. I make it my goal to
+            create software with the user in mind, creating applications with a useable and intuitive user interface
+            experience. I also understand the importance of creating highly readable and easily maintainable source
+            code. I am constantly striving to learn new technologies and look to ways to better myself in this rapidly
+            changing industry.</p>
+          <hr className="my-4"/>
+          <h3>
+            <i className="fab fa-github-square"/>
+          </h3>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="container main">
+      {elements.map((value, index) => {
+        return (
+          <div className="row post" key={index}>
+            <div className="col post">
+              <img src="https://placekitten.com/400/260" alt="placeholder" className="rounded"/>
+              <div className="post-overview">
+                <h5>{faker.lorem.sentences(1)}</h5>
+                <p>{faker.random.number({ min: 0, max: 12, precision: 1 })}/{faker.random.number({
+                  min: 0,
+                  max: 30,
+                  precision: 1
+                })}/2020</p>
+                <p>{faker.lorem.paragraph(5)}</p>
+                <a href="/">Read More</a>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
   </Layout>
 )
 
