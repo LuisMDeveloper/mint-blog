13e2b3296bd93e908615e470f450535eb56c7f3b
Ramirez Vargas, Luis Manuel <LRamirezVargas@luxoft.com>
2020-05-31 17:45:07 -0500

    basic bootstrap home page WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,39 +10,44 @@ const IndexPage = () => (
     <SEO title="Home"/>
     <div className="profile-jumbotron">
       <div className="container">
-        <div className="profile-image">
-          <img src="https://placekitten.com/300/300" alt="placeholder" className="img-thumbnail rounded-circle"/>
-        </div>
-        <div className="profile-overview">
-          <h1 className="display-4">Luis Manuel Ramirez Vargas</h1>
-          <p>iOS software developer with seven years experience. I make it my goal to
-            create software with the user in mind, creating applications with a useable and intuitive user interface
-            experience. I also understand the importance of creating highly readable and easily maintainable source
-            code. I am constantly striving to learn new technologies and look to ways to better myself in this rapidly
-            changing industry.</p>
-          <hr className="my-4"/>
-          <h3>
-            <i className="fab fa-github-square"/>
-          </h3>
+        <div className="row d-flex align-items-center">
+          <div className="col-lg-4 col-md-12 col-sm-12">
+            <img src="https://placekitten.com/300/300" alt="placeholder" className="img-thumbnail rounded-circle"/>
+          </div>
+          <div className="col-lg-8 col-md-12 col-sm-12">
+            <h1 className="display-4">Luis Manuel Ramirez Vargas</h1>
+            <p>@LuisMDeveloper</p>
+            <p>iOS software developer with seven years experience. I make it my goal to
+              create software with the user in mind, creating applications with a useable and intuitive user interface
+              experience. I also understand the importance of creating highly readable and easily maintainable source
+              code. I am constantly striving to learn new technologies and look to ways to better myself in this rapidly
+              changing industry.</p>
+            <hr className="my-4"/>
+            <div className="icon-row d-flex justify-content-start">
+              <i className="fab fa-github-square fa-2x"/>
+              <i className="fab fa-twitter-square fa-2x"/>
+              <i className="fas fa-envelope-square fa-2x"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div className="container main">
       {elements.map((value, index) => {
         return (
-          <div className="row post" key={index}>
-            <div className="col post">
-              <img src="https://placekitten.com/400/260" alt="placeholder" className="rounded"/>
-              <div className="post-overview">
-                <h5>{faker.lorem.sentences(1)}</h5>
-                <p>{faker.random.number({ min: 0, max: 12, precision: 1 })}/{faker.random.number({
-                  min: 0,
-                  max: 30,
-                  precision: 1
-                })}/2020</p>
-                <p>{faker.lorem.paragraph(5)}</p>
-                <a href="/">Read More</a>
-              </div>
+          <div className="row post d-flex align-items-center" key={index}>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <img src="https://placekitten.com/550/350" alt="placeholder" className="img-fluid rounded"/>
+            </div>
+            <div className="col-lg-8 col-md-6 col-sm-12">
+              <h5>{faker.lorem.sentences(1)}</h5>
+              <p>{faker.random.number({ min: 0, max: 12, precision: 1 })}/{faker.random.number({
+                min: 0,
+                max: 30,
+                precision: 1
+              })}/2020</p>
+              <p>{faker.lorem.paragraph(5)}</p>
+              <a href="/">Read More</a>
             </div>
           </div>
         )
